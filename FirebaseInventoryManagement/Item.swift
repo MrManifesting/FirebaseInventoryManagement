cbd1c958496fb65b1c2618c5c9ed626c85ceac1a
NICHOLAS KELLEY <whateverscleverboss@icloud.com>
2025-01-03 13:28:29 -0800

    Initial Commit

---
+++ FirebaseInventoryManagement/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FirebaseInventoryManagement
+//
+//  Created by Nick Kelley on 1/3/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FirebaseInventoryManagementTests/FirebaseInventoryManagementTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseInventoryManagementTests.swift
+//  FirebaseInventoryManagementTests
+//
+//  Created by Nick Kelley on 1/3/25.
+//
+
+import Testing
+@testable import FirebaseInventoryManagement
+
+struct FirebaseInventoryManagementTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
